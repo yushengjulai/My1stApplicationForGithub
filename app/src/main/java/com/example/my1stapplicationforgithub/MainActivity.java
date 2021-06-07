9e95d4bdf9c7f0ee22d9d0b9cbeb4db7e24d5c8d
yushengjulai <398725188@qq.com>
2021-06-07 11:22:58 +0800

    060703

--- app/src/main/java/com/example/my1stapplicationforgithub/MainActivity.java
+++ app/src/main/java/com/example/my1stapplicationforgithub/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.my1stapplicationforgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
         RVData data = new RVData();
         data.setId(1);
         data.setName("第一个");
+        TextView text1 = findViewById(R.id.textView);
+        TextView text2 = findViewById(R.id.textView2);
+        text1.setText(data.getId());
+        text2.setText(data.getName());
     }
 }
